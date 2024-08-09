f95723c66249a063d78ca7ca7ff26c768f502d8c
Arlene <149131518+arlomoney@users.noreply.github.com>
2024-08-09 13:13:35 -0700

    Update and rename Personality.java to KeirseyTemperamentSorter.java

--- KeirseyTemperamentSorter.java
+++ KeirseyTemperamentSorter.java
@@ -1,13 +1,7 @@
-// Arlene Shergill
-// January 19th, 2024
-// AP CSA
-// Mr. Thompson
-// Project #6 - Personality Project
-
 import java.io.*;
 import java.util.*;
 
-public class Personality {
+public class KeirseyTemperamentSorterQuiz {
 	
 	public static int DIMENSION = 4;
 	
@@ -15,7 +9,7 @@ public class Personality {
 		
 		intro();
 		
-		Scanner scan = new Scanner(System.in); //gets the name of the input/output files
+		Scanner scan = new Scanner(System.in);
 		
 		System.out.print("input file name? ");
 		Scanner inputFile = new Scanner(new File(scan.next()));
@@ -38,44 +32,36 @@ public class Personality {
 		System.out.println();
 	}
 	
-   //method that reports the final score and the personality type in the output file using the data from the input file
-   //precond: the input file name and the output file name
-   //postcond: prints the name, scores, and the personality type to the output file
 	public static void personInfo(Scanner inputFile, PrintStream outputFile) {
 		
-		while (inputFile.hasNextLine()) { //loops through all the lines in th efile
+		while (inputFile.hasNextLine()) {
 			
 			String name = inputFile.nextLine();
 			String data = (inputFile.nextLine()).toLowerCase();
 			
-			char[] Data = data.toCharArray(); //turned it into a character array so it is easier to sort/loop through
+			char[] Data = data.toCharArray();
 			int scores[] = calculateScores(categories(Data));
 			
-			outputFile.println(name + ": " + Arrays.toString(scores) + " = " + calculateType(scores)); //prints what we see in the output file
+			outputFile.println(name + ": " + Arrays.toString(scores) + " = " + calculateType(scores)); 
 			
 		}
 	}
-   //seperates the array into its appropriate categories
-   //precond: char array of the data
-   //postcond: returns an array of data which was sorted by a and b
+
 	public static int[][] categories(char[] data) {
 		int[][] counter = new int[2][DIMENSION];
 		
 		for (int i = 0; i < data.length; i++) {
 			
 			if (data[i] == 'a') {
-				counter[0][(i % 7 + 1) / 2] += 1; //enters the data in the first dimension of the array
+				counter[0][(i % 7 + 1) / 2] += 1; 
 				
 			} else if (data[i] == 'b') {
-				counter[1][(i % 7 + 1) / 2] += 1; //enters the data into the second dimension of the array
+				counter[1][(i % 7 + 1) / 2] += 1;
 			}
 		}
 		return counter;
 	}
 	
-   //method that calculates the b percentage of each category
-   //precond: 2 dimension array of values
-   //postcond: the percentages of each category
 	public static int[] calculateScores(int counter[][]) {
 		
 		int[] scores = new int[DIMENSION];
@@ -87,17 +73,14 @@ public class Personality {
 		return scores;
 	}
 	
-   //assigns the personality type
-   //precond: array of percentages of categories
-   //postcond: the personality type
 	public static String calculateType(int[] scores) {
 		
-		String[][] dimensions ={{"E", "S", "T", "J"}, {"I", "N", "F", "P"}}; //two indexes, seperates the oppsite types
+		String[][] dimensions ={{"E", "S", "T", "J"}, {"I", "N", "F", "P"}}; 
 		String type = "";
 		
 		for (int i = 0; i < DIMENSION; i++) {
 			
-			if (scores[i] > 50) { //assigns the type based off if b was over or under 50%
+			if (scores[i] > 50) {
 				type += dimensions[1][i];
 				
 			} else if (scores[i] < 50) { 
